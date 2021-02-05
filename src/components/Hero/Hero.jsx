a9d41cd2c9a52f30d71d424de84f27e89577d07c
Shay <smkelley91@gmail.com>
2021-02-05 08:12:30 -0600

    adding hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,10 @@
+import coffee from "../../images/coffee.jpeg"
+import './Hero.css'
+
 export default function Hero() {
   return (
     <div className="hero">
-      
+      <img src={ coffee }/>
     </div>
   )
 }
