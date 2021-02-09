427c2cda01f5d53136c2e1a439446577370e7f8a
Shay <smkelley91@gmail.com>
2021-02-09 15:08:58 -0600

    added puzzle image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import { NavLink } from "react-router-dom";
 import "./Header.css";
+import puzzle from "../../images/jiggy.png"
 
 export default function Header() {
   return (
     <div className="header">
-      <NavLink to="" className="header-name">Shay Kelley</NavLink>
+      <div className="header-name">
+        <img src={ puzzle }/>
+      <NavLink to="" >
+          Shay Kelley
+      </NavLink>
+      </div>
       <div className='nav-container'>
         <NavLink to="">About</NavLink>
         <NavLink to="">Projects</NavLink>
