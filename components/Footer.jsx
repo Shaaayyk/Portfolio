315725ecb0218eede46e2871acc138fda99da2d7
Shay Kelley <shaaayyk@gmail.com>
2024-01-11 01:52:52 -0600

    added classnames for footer to work with dark mode

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer(){
   return (
     <div>
-      <h4>&copy; 2024 Shay Kelley</h4>
+      <h4 className="text-md py-5 text-gray-800 max-w-xl md:text-lg dark:text-gray-200">
+        &copy; 2024 Shay Kelley
+      </h4>
     </div>
   )
 }
